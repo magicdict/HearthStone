4655b5fd7b6097b8a14a6babadc95de5cc6ea523
MagicHu <huweimin@cn.ibm.com>
2014-05-26 12:13:52 +0800

    Init

---
+++ Card/AbilityCard.cs
@@ -0,0 +1,18 @@
+﻿using Card.Effect;
+using System;
+using System.Collections.Generic;
+
+namespace Card
+{
+    /// <summary>
+    /// 法术卡牌
+    /// </summary>
+    [Serializable]
+    public class AbilityCard : CardBasicInfo
+    {
+        /// <summary>
+        /// 法术
+        /// </summary>
+        public Ability CardAbility = new Ability();
+    }
+}
