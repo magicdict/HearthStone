fab5c35e84e4a237f4ca045a0b226d756caf3d1c
MagicHu <huweimin@cn.ibm.com>
2014-08-05 11:00:26 +0800

    开始文档的编写工作

--- Engine/Card/Buff.cs
+++ Engine/Card/Buff.cs
@@ -1,6 +1,8 @@
 ﻿using Engine.Action;
+using Engine.Effect;
 using Engine.Utility;
 using System;
+using System.Collections.Generic;
 
 namespace Engine.Card
 {
@@ -72,7 +74,6 @@ namespace Engine.Card
             game.AllRole.MyPublicInfo.BattleField.AbilityCost = 0;
             game.AllRole.MyPublicInfo.BattleField.AbilityDamagePlus = 0;
             game.AllRole.MyPublicInfo.BattleField.MinionCost = 0;
-
             game.AllRole.YourPublicInfo.BattleField.AbilityCost = 0;
             game.AllRole.YourPublicInfo.BattleField.AbilityDamagePlus = 0;
             game.AllRole.YourPublicInfo.BattleField.MinionCost = 0;
@@ -81,80 +82,73 @@ namespace Engine.Card
             for (int i = 0; i < game.AllRole.MyPublicInfo.BattleField.MinionCount; i++)
             {
                 var minion = game.AllRole.MyPublicInfo.BattleField.BattleMinions[i];
-                if (minion != null)
+                if (minion != null && !string.IsNullOrEmpty(minion.光环效果.信息))
                 {
-                    if (!string.IsNullOrEmpty(minion.光环效果.信息))
+                    switch (minion.光环效果.类型)
                     {
-                        switch (minion.光环效果.类型)
-                        {
-                            case 光环类型枚举.增加攻防:
-                                switch (minion.光环效果.范围.EffictTargetSelectMode)
-                                {
-                                    case CardUtility.目标选择模式枚举.全体:
-                                        for (int j = 0; j < game.AllRole.MyPublicInfo.BattleField.BattleMinions.Length; j++)
-                                        {
-                                            if (game.AllRole.MyPublicInfo.BattleField.BattleMinions[j] != null)
-                                                game.AllRole.MyPublicInfo.BattleField.BattleMinions[j].受战场效果.Add(minion.光环效果);
-                                        }
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                break;
-                            case 光环类型枚举.施法成本:
-                                switch (minion.光环效果.范围.EffectTargetSelectDirect)
-                                {
-                                    case CardUtility.目标选择方向枚举.本方:
-                                        game.AllRole.MyPublicInfo.BattleField.AbilityCost += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    case CardUtility.目标选择方向枚举.对方:
-                                        game.AllRole.YourPublicInfo.BattleField.AbilityCost += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    case CardUtility.目标选择方向枚举.双方:
-                                        game.AllRole.MyPublicInfo.BattleField.AbilityCost += int.Parse(minion.光环效果.信息);
-                                        game.AllRole.YourPublicInfo.BattleField.AbilityCost += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                break;
-                            case 光环类型枚举.法术效果:
-                                switch (minion.光环效果.范围.EffectTargetSelectDirect)
-                                {
-                                    case CardUtility.目标选择方向枚举.本方:
-                                        game.AllRole.MyPublicInfo.BattleField.AbilityDamagePlus += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    case CardUtility.目标选择方向枚举.对方:
-                                        game.AllRole.YourPublicInfo.BattleField.AbilityDamagePlus += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    case CardUtility.目标选择方向枚举.双方:
-                                        game.AllRole.MyPublicInfo.BattleField.AbilityDamagePlus += int.Parse(minion.光环效果.信息);
-                                        game.AllRole.YourPublicInfo.BattleField.AbilityDamagePlus += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                break;
-                            case 光环类型枚举.随从成本:
-                                switch (minion.光环效果.范围.EffectTargetSelectDirect)
-                                {
-                                    case CardUtility.目标选择方向枚举.本方:
-                                        game.AllRole.MyPublicInfo.BattleField.MinionCost += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    case CardUtility.目标选择方向枚举.对方:
-                                        game.AllRole.YourPublicInfo.BattleField.MinionCost += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    case CardUtility.目标选择方向枚举.双方:
-                                        game.AllRole.MyPublicInfo.BattleField.MinionCost += int.Parse(minion.光环效果.信息);
-                                        game.AllRole.YourPublicInfo.BattleField.MinionCost += int.Parse(minion.光环效果.信息);
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                break;
-                            default:
-                                break;
-                        }
+                        case 光环类型枚举.增加攻防:
+                            EffectDefine singleEffect = new EffectDefine();
+                            PointEffect t = new PointEffect();
+                            singleEffect.TrueAtomicEffect = new AtomicEffectDefine();
+                            singleEffect.TrueAtomicEffect.AtomicEffectType = AtomicEffectDefine.AtomicEffectEnum.增益;
+                            singleEffect.TrueAtomicEffect.InfoArray = new List<String>(minion.光环效果.信息.Split(CardUtility.strSplitArrayMark.ToCharArray()));
+                            singleEffect.FalseAtomicEffect = new AtomicEffectDefine();
+                            singleEffect.FalseAtomicEffect.AtomicEffectType = AtomicEffectDefine.AtomicEffectEnum.未定义;
+                            Effecthandler.RunSingleEffect(singleEffect, game, 0);
+                            break;
+                        case 光环类型枚举.施法成本:
+                            switch (minion.光环效果.范围.EffectTargetSelectDirect)
+                            {
+                                case CardUtility.目标选择方向枚举.本方:
+                                    game.AllRole.MyPublicInfo.BattleField.AbilityCost += int.Parse(minion.光环效果.信息);
+                                    break;
+                                case CardUtility.目标选择方向枚举.对方:
+                                    game.AllRole.YourPublicInfo.BattleField.AbilityCost += int.Parse(minion.光环效果.信息);
+                                    break;
+                                case CardUtility.目标选择方向枚举.双方:
+                                    game.AllRole.MyPublicInfo.BattleField.AbilityCost += int.Parse(minion.光环效果.信息);
+                                    game.AllRole.YourPublicInfo.BattleField.AbilityCost += int.Parse(minion.光环效果.信息);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        case 光环类型枚举.法术效果:
+                            switch (minion.光环效果.范围.EffectTargetSelectDirect)
+                            {
+                                case CardUtility.目标选择方向枚举.本方:
+                                    game.AllRole.MyPublicInfo.BattleField.AbilityDamagePlus += int.Parse(minion.光环效果.信息);
+                                    break;
+                                case CardUtility.目标选择方向枚举.对方:
+                                    game.AllRole.YourPublicInfo.BattleField.AbilityDamagePlus += int.Parse(minion.光环效果.信息);
+                                    break;
+                                case CardUtility.目标选择方向枚举.双方:
+                                    game.AllRole.MyPublicInfo.BattleField.AbilityDamagePlus += int.Parse(minion.光环效果.信息);
+                                    game.AllRole.YourPublicInfo.BattleField.AbilityDamagePlus += int.Parse(minion.光环效果.信息);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        case 光环类型枚举.随从成本:
+                            switch (minion.光环效果.范围.EffectTargetSelectDirect)
+                            {
+                                case CardUtility.目标选择方向枚举.本方:
+                                    game.AllRole.MyPublicInfo.BattleField.MinionCost += int.Parse(minion.光环效果.信息);
+                                    break;
+                                case CardUtility.目标选择方向枚举.对方:
+                                    game.AllRole.YourPublicInfo.BattleField.MinionCost += int.Parse(minion.光环效果.信息);
+                                    break;
+                                case CardUtility.目标选择方向枚举.双方:
+                                    game.AllRole.MyPublicInfo.BattleField.MinionCost += int.Parse(minion.光环效果.信息);
+                                    game.AllRole.YourPublicInfo.BattleField.MinionCost += int.Parse(minion.光环效果.信息);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        default:
+                            break;
                     }
                 }
             }
